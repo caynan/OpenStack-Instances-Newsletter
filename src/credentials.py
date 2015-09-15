90111b1bc29d7044f12ec35f431f65efc9da636d
artxdmaster <arthur.rodrigues@ccc.ufcg.edu.br>
2015-09-15 14:27:47 +0000

    Changes

--- src/credentials.py
+++ src/credentials.py
@@ -1,18 +1,30 @@
 import os
 
+username = os.environ.get('OS_USERNAME')
+password = os.environ.get('OS_PASSWORD')
+url = os.environ.get('OS_AUTH_URL')
+project = os.environ.get('OS_TENANT_NAME')
+
 def get_nova_credentials():
-   	d = {}
-	d['version'] = '2'
-	d['username'] = os.environ['OS_USERNAME']
-	d['api_key'] = os.environ['OS_PASSWORD']
-	d['auth_url'] = os.environ['OS_AUTH_URL']
-	d['project_id'] = os.environ['OS_TENANT_NAME']
+   	check_variables()
+
+	d = {}	
+	d['username'] = username 
+	d['api_key'] = password
+	d['auth_url'] = url
+	d['project_id'] = project
 	return d
 
 def get_keystone_credentials():
+	check_variables()
+
 	d = {}
-    	d['username'] = os.environ['OS_USERNAME']
-    	d['password'] = os.environ['OS_PASSWORD']
-    	d['auth_url'] = os.environ['OS_AUTH_URL']
-    	d['tenant_name'] = os.environ['OS_TENANT_NAME']
+    	d['username'] = username
+    	d['password'] = password
+    	d['auth_url'] = url
+    	d['tenant_name'] = project
     	return d
+
+def check_variables():
+	if username == None or password == None or url == None or project == None:
+		raise EnvironmentError("Set environment variables.")
