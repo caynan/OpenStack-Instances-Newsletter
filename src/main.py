c8028dc509202605e7254c7ca6c3de7bc5e31bd5
Diego Adolfo <diegoado@gmail.com>
2015-09-16 15:36:34 +0000

    Add function to delete a instance by your id

--- src/main.py
+++ src/main.py
@@ -1,16 +1,25 @@
+import re
+
 from emailx import *
 from credentials import *
 from novaclient.v2 import client as novaclient
 from keystoneclient.v2_0 import client as keystoneclient
 
-def get_users(keystone, nova):
+kwargs = get_keystone_credentials()
+keystone = keystoneclient.Client(**kwargs)
+
+kwargs = get_nova_credentials()
+nova = novaclient.Client(**kwargs)
+
+def get_users():
     kwargs = {}
     users = keystone.users.list()
-    servers = get_servers(keystone, nova)    
+    servers = get_servers()    
 	
     for user in users:
 	try:
-	    kwargs[user.username] = {'id': user.id, 'email': user.email}
+	    if re.match(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$", user.email):
+	       kwargs[user.username] = {'id': user.id, 'email': user.email}
 	    if servers.has_key(user.id):
 	       kwargs[user.username].update({'servers': servers[user.id]})
 	except AttributeError:
@@ -18,26 +27,15 @@ def get_users(keystone, nova):
 
     return kwargs
 
-def get_projects(keystone):
+
+def get_servers():
     kwargs = {}
-    projects = keystone.tenants.list()
-
-    for project in projects:
-	kwargs[project.id] = project.name
-
-    return kwargs
-
-def get_servers(keystone, nova):
-    kwargs = {}
-    projects = get_projects(keystone)
     servers = nova.servers.list(search_opts={'all_tenants': 1})
     for server in servers:
 	flavor = nova.flavors.find(id=server.flavor['id'])
 	if kwargs.has_key(server.user_id):
 	     kwargs[server.user_id].update({server.name: {
 					'id': server.id,
-					'project_id': server.tenant_id,
-					'project_name': projects[server.tenant_id],
 					'cpu': '%d %s' % (flavor.vcpus, 'vCPUs' if flavor.vcpus > 1 else 'vCPU'), 
 					'ram': '%d MB' % flavor.ram,	
 					'created': '%s' % server.created[:10],
@@ -45,8 +43,6 @@ def get_servers(keystone, nova):
 	else:
 	     kwargs[server.user_id] = {server.name: {
 				       'id': server.id,
-				       'project_id': server.tenant_id,  
-				       'project_name': projects[server.tenant_id],
                                        'cpu': '%d %s' % (flavor.vcpus, 'vCPUs' if flavor.vcpus > 1 else 'vCPU'),
                                        'ram': '%d MB' % flavor.ram,
                                        'created': '%s' % server.created[:10],
@@ -54,13 +50,13 @@ def get_servers(keystone, nova):
 
     return kwargs
 
-def main():
-    _creds = get_keystone_credentials()
-    keystone = keystoneclient.Client(**_creds)
-    _creds = get_nova_credentials()
-    nova = novaclient.Client(**_creds)
 
-    users = get_users(keystone, nova)
+def delete_server(server_id):
+    nova.servers.delete(server_id)
+
+
+def main():
+    users = get_users()
     send_email(users)
 
 if __name__ == '__main__':
