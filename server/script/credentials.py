5068c6353a79809099e35e6c574e5cd796ef1cf3
Diego Adolfo <diegoado@gmail.com>
2015-09-17 14:33:46 +0000

    Add new form to load admin credentials

--- server/script/credentials.py
+++ server/script/credentials.py
@@ -1,22 +1,47 @@
 import os
+import json
+import subprocess
 
-username = os.environ.get('OS_USERNAME')
 password = os.environ.get('OS_PASSWORD')
-url = os.environ.get('OS_AUTH_URL')
 project = os.environ.get('OS_TENANT_NAME')
+url = os.environ.get('OS_AUTH_URL')
+username = os.environ.get('OS_USERNAME')
 
 def get_nova_credentials():
-	d = {}	
-	d['username'] = username 
-	d['api_key'] = password
-	d['auth_url'] = url
-	d['project_id'] = project
-	return d
+	set_environments()
+	args = {}	
+	args['username'] = username 
+	args['api_key'] = password
+	args['auth_url'] = url
+	args['project_id'] = project
+	return args
+
 
 def get_keystone_credentials():
-	d = {}
-    	d['username'] = username
-    	d['password'] = password
-    	d['auth_url'] = url
-    	d['tenant_name'] = project
-    	return d
+	set_environments()
+	args = {}
+    	args['username'] = username
+    	args['password'] = password
+    	args['auth_url'] = url
+    	args['tenant_name'] = project
+	return args
+
+
+def set_environments():
+    global password
+    global project
+    global username
+    global url
+
+    if password is None or project is None or username is None or url is None:
+       source = 'source openrc.sh'
+       dump = '/usr/bin/python -c "import os, json; print json.dumps(dict(os.environ))"'
+       popen = subprocess.Popen(['/bin/bash', '-c', '%s && %s' % (source, dump)], stdout=subprocess.PIPE)
+       env = json.loads(popen.stdout.read())
+       os.environ = env	
+
+    password = os.environ.get('OS_PASSWORD')
+    project = os.environ.get('OS_TENANT_NAME')
+    url = os.environ.get('OS_AUTH_URL')
+    username = os.environ.get('OS_USERNAME')
+
