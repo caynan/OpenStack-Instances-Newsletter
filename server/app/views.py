b0573f87cbec9d046ba73c76bb2aac7a9b8c1b59
Diego Adolfo <diegoado@gmail.com>
2015-09-21 18:22:39 +0000

    building html confirm deleted instance in server routes

--- server/app/views.py
+++ server/app/views.py
@@ -3,5 +3,14 @@ from script.main import *
 
 @app.route("/instances/delete/<id>")
 def instance_delete(id):
-    delete_server(server_hash=id)
-    return "Instance deleted successfully"
+    try:
+	delete_server(server_hash=id)
+	return "<h1>Instance deleted successfully</h1>"
+    except ValueError:
+    	return "<h1>Instance deleted successfully</h1>"
+
+
+@app.route("/instances/confirm/<id>")
+def confirm_action(hash):
+    pass
+
